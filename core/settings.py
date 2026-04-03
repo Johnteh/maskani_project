e1a9f85a8b40b00ea870541ca5221abbcbc0cca6
John Miiri <john.miiri@savannahinformatics.com>
2026-04-03 15:26:42 +0300

    feat: add postgres

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +77,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+d = dj_database_url.parse(
+    os.getenv("DATABASE_URL", "postgres://maskani_user:maskani_pass@localhost:5432/maskani_db")
+)
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": d,
 }
 
 
