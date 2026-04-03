20755d9a23ca8133d65fd7dda9874bc07f73fb57
John Miiri <john.miiri@savannahinformatics.com>
2026-04-03 15:54:47 +0300

    feat: add allauth

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # allauth
+    'allauth',
+    'allauth.account',
 
     # third parties
     'rest_framework',
